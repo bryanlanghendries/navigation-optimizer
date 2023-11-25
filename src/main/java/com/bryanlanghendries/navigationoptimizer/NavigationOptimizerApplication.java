8216923c7d46af2f7f5c80f9b05ac716dfbba7cb
Bryan Langhendries <bryan.langhendries@student.ehb.be>
2023-11-25 11:16:09 +0100

    Setup

---
+++ src/main/java/com/bryanlanghendries/navigationoptimizer/NavigationOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.bryanlanghendries.navigationoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavigationOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavigationOptimizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bryanlanghendries/navigationoptimizer/NavigationOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryanlanghendries.navigationoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavigationOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
